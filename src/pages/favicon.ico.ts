01038a31d73a1fbaf451d49bf4a441b91303acee
RTaylor <designer.racheltaylor@gmail.com>
2024-06-21 12:58:00 -0700

    Try direct import of favicon asset

--- src/pages/favicon.ico.ts
+++ src/pages/favicon.ico.ts
@@ -3,11 +3,10 @@ import path from "node:path";
 import type { APIRoute } from "astro";
 import sharp from "sharp";
 import ico from "sharp-ico";
-
-const faviconSrc = path.resolve(`src/assets/logos/logo.png`);
+import favicon from "../assets/logos/logo.png"
 
 export const GET: APIRoute = async () => {
-  const buffer = await sharp(faviconSrc).resize(32).toFormat(`png`).toBuffer();
+  const buffer = await sharp(favicon.src).resize(32).toFormat(`png`).toBuffer();
   const icoBuffer = ico.encode([buffer]);
 
   return new Response(icoBuffer, {
