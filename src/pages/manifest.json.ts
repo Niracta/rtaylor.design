3dd5ed8976439181113103a328ba2ac97b359b25
Drake Costa <drake@saeris.io>
2024-06-20 22:42:53 -0700

    Add favicon and manifest metadata, Switch to Fontsource dependencies

---
+++ src/pages/manifest.json.ts
@@ -0,0 +1,37 @@
+import type { APIRoute } from "astro";
+import { getImage } from "astro:assets";
+import favicon from "../assets/logos/logo.png";
+
+const faviconPngSizes = [192, 512];
+
+export const GET: APIRoute = async () => {
+  const icons = await Promise.all(
+    faviconPngSizes.map(async (size) => {
+      const image = await getImage({
+        src: favicon,
+        width: size,
+        height: size,
+        format: `png`
+      });
+      return {
+        src: image.src,
+        type: `image/${image.options.format}`,
+        sizes: `${image.options.width}x${image.options.height}`
+      };
+    })
+  );
+
+  const manifest = {
+    name: `Rachel Taylor Design`,
+    short_name: `Rachel Taylor Design`,
+    description: `Personal graphic design and illustration portfolio of Rachel Taylor.`,
+    start_url: `/`,
+    background_color: `#fff`,
+    theme_color: `#D9027D`,
+    display: `standalone`,
+    icon: `/logos/logo.png`,
+    icons
+  };
+
+  return new Response(JSON.stringify(manifest));
+};
--- src/styles/vendor.ts
+++ src/styles/vendor.ts
@@ -1,4 +1,6 @@
-import "typeface-montserrat";
+import "@fontsource-variable/montserrat";
+import "@fontsource/varela-round";
+import "@fontsource-variable/work-sans";
 import "remark-alerts/styles/github-colors-light.css";
 import "remark-alerts/styles/github-colors-dark-media.css";
 import "remark-alerts/styles/github-base.css";
